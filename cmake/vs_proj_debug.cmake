bca52f2d861e6451fbe6da6d69d683b4c094c3bd
zhang xiao bin <zhangxiaobin@ztgame.com>
2020-08-26 15:59:45 +0800

    modfile pro dir

--- cmake/vs_proj_debug.cmake
+++
@@ -1,6 +0,0 @@
-function(vs_proj_debug)
-    if(BUILD_OS MATCHES win)
-        set(NODE "${CMAKE_SOURCE_DIR}/tools/node/node.exe")
-        gamesh_execute(COMMAND "${NODE}" "${CMAKE_SOURCE_DIR}/tools/node_scripts/vs_proj_debug.js" )
-    endif()
-endfunction(vs_proj_debug)
