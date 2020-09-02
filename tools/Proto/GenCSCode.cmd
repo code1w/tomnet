65e95042326c834c8f8f5d3248b38175e450d581
zhang xiao bin <zhangxiaobin@ztgame.com>
2020-09-02 20:46:18 +0800

    mod ...

--- tools/Proto/GenCSCode.cmd
+++ tools/Proto/GenCSCode.cmd
@@ -1,2 +1,2 @@
 protoc_3_12_3_static --proto_path=./src  --csharp_out=./src ./src/*.proto
-protoc_3_12_3_dynamic --proto_path=./src  --cpp_out=./src ./src/*.proto
+protoc_3_13_0_static --proto_path=./src  --cpp_out=./src ./src/*.proto
