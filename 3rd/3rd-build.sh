c7c876abd2920251b82d3206d4cb0c2a7f3c2123
root <root@LAPTOP-0FV64FQN.localdomain>
2020-11-19 00:15:52 +0800

    删除tbb

--- 3rd/3rd-build.sh
+++ 3rd/3rd-build.sh
@@ -6,21 +6,23 @@ cd $dir
 echo "shell build protobuf ...."
 if [ ! -d "protobuf-3.12.0" ]; then
     tar zxf protobuf-all-3.12.0.tar.gz
+    cd protobuf-3.12.0
+    ./configure
+    make -j4
+    cd ..
 fi
-cd protobuf-3.12.0
-./configure
-make -j4
 
-cd ..
+
+
 
 echo "shell build jemalloc ...."
 if [ ! -d "jemalloc-5.2.1" ]; then
     tar zxf jemalloc-5.2.1.tar.gz
+    cd jemalloc-5.2.1
+    ./autogen.sh
+    ./configure --with-jemalloc-prefix=je_
+    make -j4
 fi
-cd jemalloc-5.2.1
-./autogen.sh
-./configure --with-jemalloc-prefix=je_
-make -j4
 
 
 
